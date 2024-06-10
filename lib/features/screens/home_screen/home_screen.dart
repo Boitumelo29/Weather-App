cdda277ce9b44a0af50f35bdb926347e9c92c338
Boitumelo Thobejane <tumijane99@gmail.com>
2024-06-10 08:54:26 +0200

    update

--- lib/features/screens/home_screen/home_screen.dart
+++ lib/features/screens/home_screen/home_screen.dart
@@ -118,7 +118,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                         // stops: const [
                                         //   0.1,
                                         //   0.4,
-                                        // updat
+                                        // upd
                                         // ],
                                         colors: [
                                           Colors.blue,
